599878ffd57ff6a25578c9832a9e0e3d1fdd88e2
Noorthistime <noorsayyed.atwork@gmail.com>
2026-07-09 18:00:57 +0530

    Increased the file size limit

---
+++ src/main/java/com/project/p2p/controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.project.p2p.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "File is too large! Maximum allowed size is 500MB.");
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(response);
+    }
+}
